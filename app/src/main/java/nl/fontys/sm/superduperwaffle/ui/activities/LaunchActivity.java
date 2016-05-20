e7107467dc9eba29a1ca13396f688aa468b71cc4
Requinard <d.diks94@gmail.com>
2016-05-20 10:35:22 +0200

    Added launch activity

---
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LaunchActivity.java
@@ -0,0 +1,16 @@
+package nl.fontys.sm.superduperwaffle.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import nl.fontys.sm.superduperwaffle.R;
+
+/**
+ * Created by David on 5/20/2016.
+ */
+public class LaunchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
