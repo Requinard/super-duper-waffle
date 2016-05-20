f5ed11e7b4969325e562c5cc1b398f70ef66e0e7
David Diks <d.diks94@gmail.com>
2016-05-20 10:05:29 +0200

    Added home activity

---
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/HomeActivity.java
@@ -0,0 +1,17 @@
+package nl.fontys.sm.superduperwaffle.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import nl.fontys.sm.superduperwaffle.R;
+
+/**
+ * Created by David on 5/20/2016.
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
