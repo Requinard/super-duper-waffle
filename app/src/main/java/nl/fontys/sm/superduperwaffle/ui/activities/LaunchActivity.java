cba2a204547a5a6256c8bff466770a07087c3a34
Minh-Triet Diep <minhtrietdiep@gmail.com>
2016-05-27 10:28:22 +0200

    Example of Calendar class usage

--- app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LaunchActivity.java
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LaunchActivity.java
@@ -1,8 +1,18 @@
 package nl.fontys.sm.superduperwaffle.ui.activities;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.List;
+
 import nl.fontys.sm.superduperwaffle.R;
+import nl.fontys.sm.superduperwaffle.calendar.CalendarItem;
+import nl.fontys.sm.superduperwaffle.calendar.CalendarReader;
 
 /**
  * Created by David on 5/20/2016.
@@ -12,5 +22,30 @@ public class LaunchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        findViewById(R.id.testButton).setOnClickListener(handleClick);
     }
+
+    // Example of how the calendar events for the next months can be retrieved, containing the
+    // string "canvas" as calendar ID.
+    private View.OnClickListener handleClick = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            CalendarReader calendarReader = new CalendarReader();
+            Calendar calendar = Calendar.getInstance();
+            Calendar next = Calendar.getInstance();
+            next.add(Calendar.DATE, 30);
+            List<CalendarItem> calendarItems = calendarReader.getEvents(
+                    LaunchActivity.this,
+                    calendar,
+                    next);
+            for(CalendarItem calendarItem:calendarItems) {
+                if (calendarItem.calendarName.toLowerCase().contains("canvas")) {
+                    Log.i("Calendar: ",
+                            "Subject: " + calendarItem.subjectName + "\n" +
+                                    "Description: " + calendarItem.description + "\n"
+                    );
+                }
+            }
+        }
+    };
 }
