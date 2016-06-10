eddb8078ffae548a2a2e093ba9aad141f3082ab2
Minh-Triet Diep <minhtrietdiep@gmail.com>
2016-06-10 11:24:44 +0200

    Add Import Calendar activity

--- app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LaunchActivity.java
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LaunchActivity.java
@@ -2,6 +2,7 @@ package nl.fontys.sm.superduperwaffle.ui.activities;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,33 +31,8 @@ public class LaunchActivity extends Activity {
     // string "canvas" as calendar ID.
     private View.OnClickListener handleClick = new View.OnClickListener() {
         public void onClick(View arg0) {
-            Log.i("Debug: ", "Click");
-
-            CalendarReader calendarReader = new CalendarReader();
-            Calendar calendar = Calendar.getInstance();
-            Calendar next = Calendar.getInstance();
-            next.add(Calendar.DATE, 30);
-            Log.i("Debug: ", "Got Cal+30");
-
-            List<CalendarItem> calendarItems = calendarReader.getEvents(
-                    LaunchActivity.this,
-                    calendar,
-                    next,
-                    "canvas");
-            Log.i("Debug: ", "Got Calendar");
-
-            for(CalendarItem calendarItem : calendarItems) {
-                /*if (calendarItem.calendarName.toLowerCase().contains("canvas")) {
-                    Log.i("Calendar: ",
-                            "Subject: " + calendarItem.subjectName + "\n" +
-                                    "Description: " + calendarItem.description + "\n"
-                    );
-                }*/
-                Log.i("Calendar: ",
-                        "Subject: " + calendarItem.subjectName + "\n" +
-                                "Description: " + calendarItem.description + "\n"
-                );
-            }
+            Intent intent = new Intent(LaunchActivity.this, ImportCalendarActivity.class);
+            startActivity(intent);
         }
     };
 }
