0b19b9ad22f257deb27bafe89f26bd9775111564
requinard <d.diks94@gmail.com>
2016-06-22 10:00:11 +0200

    working on more data stuff

---
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/db/models/experimental/Finder.java
@@ -0,0 +1,12 @@
+package nl.fontys.sm.superduperwaffle.db.models.experimental;
+
+/**
+ * Created by David on 6/10/2016.
+ */
+public class Finder {
+    public String key;
+
+    public void check(Model model){
+
+    }
+}
