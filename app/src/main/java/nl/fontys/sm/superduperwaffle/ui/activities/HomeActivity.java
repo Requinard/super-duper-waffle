b259df1749facf67066d545d6c8299d6ea4b9baa
requinard <d.diks94@gmail.com>
2016-06-22 10:43:32 +0200

    Made application bootable

--- app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/HomeActivity.java
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/HomeActivity.java
@@ -1,14 +1,11 @@
 package nl.fontys.sm.superduperwaffle.ui.activities;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.Calendar;
-
 import nl.fontys.sm.superduperwaffle.R;
 
 /**
@@ -21,44 +18,49 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button btnLogin = (Button)findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(handleClickCalender );
+        Button btnCalendar = (Button) findViewById(R.id.btnBekijkKalender);
+        btnCalendar.setOnClickListener(handleClickCalender);
 
-        Button btnPlanTaak = (Button)findViewById(R.id.btnPlanTaak);
-        btnLogin.setOnClickListener(handleClickPlanTaak );
+        Button btnPlanTaak = (Button) findViewById(R.id.btnPlanTaak);
+        btnPlanTaak.setOnClickListener(handleClickPlanTaak);
 
-        Button btnMentaleToestandKlas = (Button)findViewById(R.id.btnMentaleToestandKlas);
-        btnLogin.setOnClickListener(handleClickMentaleToestandKlas );
+        Button btnMentaleToestandKlas = (Button) findViewById(R.id.btnMentaleToestandKlas);
+        btnMentaleToestandKlas.setOnClickListener(handleClickMentaleToestandKlas);
 
-        Button btnGemiddeldePerLeraar = (Button)findViewById(R.id.btnGemiddeldePerLeraar);
-        btnLogin.setOnClickListener(handleClickGemiddeldePerLeraar );
+        Button btnGemiddeldePerLeraar = (Button) findViewById(R.id.btnGemiddeldePerLeraar);
+        btnGemiddeldePerLeraar.setOnClickListener(handleClickGemiddeldePerLeraar);
     }
 
-    private View.OnClickListener handleClickCalender = new View.OnClickListener(){
-        public void onClick(View arg0){
-            Intent intent = new Intent(HomeActivity.this,CalendarActivity.class);
+    private View.OnClickListener handleClickCalender = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            Intent intent = new Intent(HomeActivity.this, CalendarActivity.class);
             startActivity(intent);
         }
     };
 
-    private View.OnClickListener handleClickPlanTaak = new View.OnClickListener(){
-        public void onClick(View arg0){
-            Intent intent = new Intent(HomeActivity.this,TaakActivity.class);
+    private View.OnClickListener handleClickPlanTaak = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            Intent intent = new Intent(HomeActivity.this, TaakActivity.class);
             startActivity(intent);
         }
     };
 
-    private View.OnClickListener handleClickMentaleToestandKlas = new View.OnClickListener(){
-        public void onClick(View arg0){
-            Intent intent = new Intent(HomeActivity.this,TaakActivity.class);
-            startActivity(intent);
+    private View.OnClickListener handleClickMentaleToestandKlas = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            Dialog dialog = new Dialog(getBaseContext());
+
+            dialog.setTitle("Deze functionaliteit is er nog niet!");
+            dialog.show();
+
         }
     };
 
-    private View.OnClickListener handleClickGemiddeldePerLeraar = new View.OnClickListener(){
-        public void onClick(View arg0){
-            Intent intent = new Intent(HomeActivity.this,TaakActivity.class);
-            startActivity(intent);
+    private View.OnClickListener handleClickGemiddeldePerLeraar = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            Dialog dialog = new Dialog(getBaseContext());
+
+            dialog.setTitle("Deze functionaliteit is er nog niet!");
+            dialog.show();
         }
     };
 
