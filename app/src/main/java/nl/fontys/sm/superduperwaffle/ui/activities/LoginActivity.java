f0f6c72549870a3b3cb4b758d158f18381d4716c
Minh-Triet Diep <minhtrietdiep@gmail.com>
2016-06-22 19:37:45 +0200

    wth mick wat is dit

--- app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LoginActivity.java
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/LoginActivity.java
@@ -5,15 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import nl.fontys.sm.superduperwaffle.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText loginName;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginName = (EditText) findViewById(R.id.tbNaam);
+        password  = (EditText) findViewById(R.id.tbWachtwoord);
 
         Button btnLogin = (Button)findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(handleClickLogin );
@@ -25,15 +31,35 @@ public class LoginActivity extends AppCompatActivity {
 
     private View.OnClickListener handleClickLogin = new View.OnClickListener(){
         public void onClick(View arg0){
-            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
-            startActivity(intent);
+            if (checkFieldsValid()) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
         }
     };
 
     private View.OnClickListener handleClickRegister = new View.OnClickListener(){
         public void onClick(View arg0){
-            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
-            startActivity(intent);
+            if (checkFieldsValid()) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
         }
     };
+
+    private boolean checkFieldsValid() {
+        if (loginName.getText().toString().isEmpty() && password.getText().toString().isEmpty()) {
+            Toast.makeText(LoginActivity.this, "No username and password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (loginName.getText().toString().isEmpty()) {
+            Toast.makeText(LoginActivity.this, "No username", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (password.getText().toString().isEmpty()) {
+            Toast.makeText(LoginActivity.this, "No password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
 }
