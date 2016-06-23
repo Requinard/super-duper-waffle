6d351db5da7c03c3f50f877c327c11120cafe8e9
jeroen0606 <jeroen0606@gmail.com>
2016-06-23 10:39:10 +0200

    Changes

--- app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/CalendarActivity.java
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/CalendarActivity.java
@@ -16,7 +16,9 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        //setContentView(R.layout.activity_import);
+
+        Button btnDeadline = (Button)findViewById(R.id.btnDeadline);
+        btnDeadline.setOnClickListener(handleClickDeadline );
     }
 
 
