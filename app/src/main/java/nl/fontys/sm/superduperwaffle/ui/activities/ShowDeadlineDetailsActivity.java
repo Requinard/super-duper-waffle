69fe4d32da5fedc53e010a106614a0a9484d946a
Minh-Triet Diep <minhtrietdiep@gmail.com>
2016-06-23 09:54:24 +0200

    Add stress data and charts! WOOP!

--- app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/ShowDeadlineDetailsActivity.java
+++ app/src/main/java/nl/fontys/sm/superduperwaffle/ui/activities/ShowDeadlineDetailsActivity.java
@@ -1,18 +1,35 @@
 package nl.fontys.sm.superduperwaffle.ui.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import nl.fontys.sm.superduperwaffle.R;
+import nl.fontys.sm.superduperwaffle.wearable.Sensor;
+import nl.fontys.sm.superduperwaffle.wearable.connectionManager;
 
 /**
  * Created by MT on 22-Jun-16.
  */
 public class ShowDeadlineDetailsActivity extends Activity {
+
+    LineChart lineChart;
+    connectionManager cm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +52,83 @@ public class ShowDeadlineDetailsActivity extends Activity {
         String dateText = df2.format(date);
         deadline.setText("Deadline: " + dateText);
 
-    }
+        lineChart = (LineChart) findViewById(R.id.lineChartCourse);
+        lineChart.setDescription("Stress levels");
+        lineChart.setNoDataTextDescription("You need to provide data for the chart.");
 
+        cm = new connectionManager();
+
+        setData(20, 100);
+
+
+    }
+    private void setData(int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            float mult = (range + 1);
+            //float val = (float) (Math.random() * mult) + 3;// + (float)
+            // ((mult *
+            // 0.1) / 10);x
+            float val = (float) cm.calculateStressLevel(
+                    Sensor.both,
+                    (double) cm.calibrateHeartRateSensor(),
+                    (double) cm.calibrateSkinConductionSensor());
+            yVals.add(new Entry(val, i));
+        }
+
+        LineDataSet set1;
+
+        if (lineChart.getData() != null &&
+                lineChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet)lineChart.getData().getDataSetByIndex(0);
+            set1.setYVals(yVals);
+            lineChart.getData().setXVals(xVals);
+            lineChart.getData().notifyDataChanged();
+
+            lineChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(yVals, "Stress levels");
+
+            // set1.setFillAlpha(110);
+            // set1.setFillColor(Color.RED);
+
+            // set the line to be drawn like this "- - - - - -"
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+
+            /*if (Utils.getSDKInt() >= 18) {
+                // fill drawable only supported on api level 18 and above
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+                set1.setFillDrawable(drawable);
+            }
+            else {*/
+                set1.setFillColor(Color.BLACK);
+            //}
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(xVals, dataSets);
+
+            // set data
+            lineChart.setData(data);
+        }
+    }
 }
